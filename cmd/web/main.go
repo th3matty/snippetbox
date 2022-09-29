7693e06126ff963a2ef348e9eb2083f4504e3b89
th3matty <matthaeus.malek@aclue.de>
2022-09-29 18:55:16 +0200

    add routing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -23,18 +23,9 @@ func main() {
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
-
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	srv := http.Server{
 		Addr:     *addr,
-		Handler:  mux,
+		Handler:  app.routes(),
 		ErrorLog: errorLog,
 	}
 
